253b07f901987319dd790b2526e5fd68e23d11af
Conga <132218618+devConga@users.noreply.github.com>
2023-09-05 11:37:06 -0300

    Actualizado damagePlayer

--- Assets/DamagePlayer.cs
+++ Assets/DamagePlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -15,6 +16,9 @@ public class DamagePlayer : MonoBehaviour
     public GameObject player;
 
     public float attackRange = 7.5f;
+
+    public bool onRange = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,17 +31,26 @@ public class DamagePlayer : MonoBehaviour
     void Update()
     {
         if(GetComponent<FieldOfView>().canSeePlayer){
-            if(!agent.pathPending && agent.remainingDistance < attackRange){
-                attackCountdown -= Time.deltaTime;
-                
-                if(attackCountdown <= 0){
-                    Destroy(player);
-                }
-            }
 
-            else{
-                attackCountdown = timeInRangeToAttack;
+            if(!agent.pathPending && agent.remainingDistance <= attackRange){
+                onRange = true;
             }
         }
+
+            if(onRange){
+                attackCountdown -= Time.deltaTime;
+                agent.speed = 0;
+                }
+
+            if(attackCountdown <= 0 && !agent.pathPending && agent.remainingDistance <= attackRange){
+                    Destroy(player);
+                }
+
+            if(attackCountdown <= 0 && !agent.pathPending && agent.remainingDistance > attackRange){
+                attackCountdown = timeInRangeToAttack;
+                onRange = false;
+                agent.speed = 6;
+            }
+        
     }
 }
